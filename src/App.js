51011706c698abaf5c2337f7dc4a90a996f9c511
pete-RK <petekanaparthi@gmail.com>
2024-07-14 19:39:30 -0700

    new commit on branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Sohail Shaik
+          Always Shaik Gandu
         </a>
       </header>
     </div>
